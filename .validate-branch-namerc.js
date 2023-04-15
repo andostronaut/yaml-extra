7bd841b7b9ec5a6486fcce97ecd41b8d50113c03
Ando RAMANAMIHANTA <iamando@tuta.io>
2023-04-15 20:39:59 +0300

    feature: use json ext for lint-staged and vbn

--- .validate-branch-namerc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  pattern:
-    '^(project-[a-z\\-]+|master|develop|preprod){1}$|^(epic|feature|bugfix|hotfix|chore|experiment|design)\\/([A-Z]{2,4})-([\\dX]+)_([\\w-]+?)$',
-  errorMsg: 'Branch name format : <type>/<issue_ID>_<name>'
-}
